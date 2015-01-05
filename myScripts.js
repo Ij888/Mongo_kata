01ec94069cff31293fbc650bfc1d2e0b29a5c99a
Ij888 <ij.ofili@gmail.com>
2015-01-05 02:15:45 +0000

    added entry
    working with mongooseJS

--- myScripts.js
+++ myScripts.js
@@ -1,1 +1,10 @@
 //my scripts. the wiring for all components reside here
+//with mongooseJS
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function(callback){
+    //yay!! 
+});
